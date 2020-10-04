13726b194c483feb2d6a73b67fba0162b638933d
Khan <daniel.khan@catenamedia.com>
2020-10-04 20:56:03 +0300

    modified main project files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/';
-import {Provider} from "react-redux";
-import store from "./store";
-import {BrowserRouter} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App/App';
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
